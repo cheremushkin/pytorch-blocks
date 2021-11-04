8533fa1d7822334fafb0ac0874189fd261296152
cheremushkin <ilya.cheremushkin@bostongene.com>
2021-11-04 03:04:01 +0300

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     install_requires = f.read().split('\n')
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 setup(
     name='pytorch_blocks',
